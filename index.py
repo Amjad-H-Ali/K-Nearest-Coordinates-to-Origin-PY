cbe85c88568a065b940bb77062726d24931ae768
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-18 21:14:56 -0600

    Defined find_nearest function

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 print("K-Nearest-Coordinates-to-Origin-PY!")
 
+# Function that takes in tuple of points and prints the "k" elements that are closest to the origin
+def find_nearest(points, k):
+	
 # Tuple representing points on a Cartesian coordinate plane 
 coordinates = [(-2, -4), (0, -2), (-1, 0), (3, -5), (-2, -3), (3, 2)]
 
