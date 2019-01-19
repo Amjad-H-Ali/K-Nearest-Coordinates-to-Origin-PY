cb55c0fcc68a9b19d1c6f6db64ef6e2889adf573
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-18 21:50:33 -0600

    Defined min heap function

--- index.py
+++ index.py
@@ -2,7 +2,10 @@ print("K-Nearest-Coordinates-to-Origin-PY!")
 
 # Function that takes in tuple of points and prints the "k" elements that are closest to the origin
 def find_nearest(points, k):
-	
+
+# Function will take in array of size "k" containing relative distances of coordinates and sort it into a min-heap
+def min_heap(array, indx):	
+
 # Tuple representing points on a Cartesian coordinate plane 
 coordinates = [(-2, -4), (0, -2), (-1, 0), (3, -5), (-2, -3), (3, 2)]
 
