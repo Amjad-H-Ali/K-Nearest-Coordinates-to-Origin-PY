fb7cb590a7d3801a4065297675c07fe37b826336
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-19 18:20:00 -0600

    Call min_heap on parent nodes of k portion of array to sort in min-heap

--- index.py
+++ index.py
@@ -3,8 +3,11 @@ print("K-Nearest-Coordinates-to-Origin-PY!")
 # Function that takes in tuple of points and prints the "k" elements that are closest to the origin
 def find_nearest(points, k):
 	
+	# Call min_heap on parent nodes of k portion of array
+	# to sort in min-heap 
 	for indx in range(int(k/2) - 1, -1, -1):
-		print(points[indx])
+		
+		min_heap(points, indx, k)
 
 
 
