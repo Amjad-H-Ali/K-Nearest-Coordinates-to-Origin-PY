2c8e4a86f3826053d76a3030884f47d97a69ca1a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-19 18:25:05 -0600

    Assume smallest relative distance belongs to the parent node coordinate

--- index.py
+++ index.py
@@ -13,7 +13,10 @@ def find_nearest(points, k):
 
 
 # Function will take in array of size "k" containing relative distances of coordinates and sort it into a min-heap
-def min_heap(array, indx, size):	
+def min_heap(array, indx, size):
+	
+	# Assume smallest relative distance belongs to the parent node coordinate
+	smallest = indx	
 
 # Tuple representing points on a Cartesian coordinate plane 
 coordinates = [(-2, -4), (0, -2), (-1, 0), (3, -5), (-2, -3), (3, 2)]
