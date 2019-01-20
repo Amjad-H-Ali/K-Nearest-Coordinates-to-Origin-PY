b180c3b0783046a0cb2f65f7198a217bb751e37c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2019-01-19 18:16:07 -0600

    Function loops through parent nodes of k portion of array

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ print("K-Nearest-Coordinates-to-Origin-PY!")
 
 # Function that takes in tuple of points and prints the "k" elements that are closest to the origin
 def find_nearest(points, k):
-
+	
+	for indx in range(int(k/2) - 1, -1, -1):
+		print(points[indx])
 
 
 
@@ -13,3 +15,4 @@ def min_heap(array, indx, size):
 # Tuple representing points on a Cartesian coordinate plane 
 coordinates = [(-2, -4), (0, -2), (-1, 0), (3, -5), (-2, -3), (3, 2)]
 
+find_nearest(coordinates, 3)
